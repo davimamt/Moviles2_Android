6756843ea0ef9db4a02b31ec5fece9f3dda6f331
505 <505@academicos.cesde.edu.co>
2022-11-22 17:46:40 -0500

    Botones y conexion bd

--- app/src/main/java/com/example/sistema_ventasfb/MainActivity.java
+++ app/src/main/java/com/example/sistema_ventasfb/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+    // Se genera un objeto para conectarse a la BD de Firebase- Firestore
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
